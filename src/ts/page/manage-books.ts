97d92d52f10f80a2c980c7bf6c0955026fbac6d2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-03 17:34:30 +0530

    Validate ISBN, Name, and Author

---
+++ src/ts/page/manage-books.ts
@@ -0,0 +1,49 @@
+const btnNewBook = document.querySelector<HTMLButtonElement>('#btn-new-book')!;
+const frmBook = document.querySelector<HTMLFormElement>('#frm-book')!;
+const txtISBN = document.querySelector<HTMLInputElement>('#txt-isbn')!;
+const txtName = document.querySelector<HTMLInputElement>('#txt-name')!;
+const txtAuthor = document.querySelector<HTMLInputElement>('#txt-author')!;
+
+setEnableForm(false);
+
+function setEnableForm(enable: boolean = true) {
+    for (const element of frmBook.elements) {
+        if (element instanceof HTMLInputElement || element instanceof HTMLButtonElement) {
+            element.disabled = !enable;
+        }
+    }
+}
+
+btnNewBook.addEventListener('click', () => {
+    setEnableForm();
+    frmBook.reset();
+    txtISBN.focus();
+});
+
+function checkValidityOfISBN(){
+    return /^\d+$/.test(txtISBN.value);
+}
+
+function checkValidityOfName(){
+    return /^.+$/.test(txtName.value);
+}
+
+function checkValidityOfAuthor(){
+    return /^[A-Za-z ]+$/.test(txtAuthor.value);
+}
+
+txtISBN.addEventListener('input', checkValidity);
+txtName.addEventListener('input', checkValidity);
+txtAuthor.addEventListener('input', checkValidity);
+
+function checkValidity(e: Event){
+    (e.target as HTMLInputElement).classList.remove('is-valid', 'is-invalid');
+    if(e.target === txtISBN){
+        checkValidityOfISBN()? txtISBN.classList.add('is-valid'): txtISBN.classList.add('is-invalid');
+    }else if (e.target === txtName){
+        checkValidityOfName()? txtName.classList.add('is-valid'): txtName.classList.add('is-invalid');
+    }else{
+        checkValidityOfAuthor()? txtAuthor.classList.add('is-valid'): txtAuthor.classList.add('is-invalid');
+    }
+}
+
