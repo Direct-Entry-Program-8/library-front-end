a97c498f5df6e16ff0c3c91a8d739e8d678935ef
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-03 18:57:49 +0530

    Finalize image browse and remove buttons

--- src/ts/page/manage-books.ts
+++ src/ts/page/manage-books.ts
@@ -4,9 +4,26 @@ const txtISBN = document.querySelector<HTMLInputElement>('#txt-isbn')!;
 const txtName = document.querySelector<HTMLInputElement>('#txt-name')!;
 const txtAuthor = document.querySelector<HTMLInputElement>('#txt-author')!;
 const btnBrowse = document.querySelector<HTMLButtonElement>('#btn-browse')!;
+const btnRemove = document.querySelector<HTMLButtonElement>('#btn-remove')!;
 const txtPreview = document.querySelector<HTMLInputElement>('#txt-preview')!;
+const divThumbnail = document.querySelector<HTMLDivElement>('#thumbnail')!;
+let blobURL: null | string = null;
 
-btnBrowse.addEventListener('click', ()=> txtPreview.click());
+btnRemove.addEventListener('click', () => {
+    txtPreview.value = '';
+    if (blobURL) URL.revokeObjectURL(blobURL);
+    blobURL = null;
+    divThumbnail.style.backgroundImage = '';
+});
+
+btnBrowse.addEventListener('click', () => txtPreview.click());
+txtPreview.addEventListener('input', () => {
+    if (txtPreview.files![0]) {
+        if (blobURL) URL.revokeObjectURL(blobURL);
+        blobURL = URL.createObjectURL(txtPreview.files![0]);
+        divThumbnail.style.backgroundImage = `url(${blobURL})`;
+    }
+});
 
 setEnableForm(false);
 
@@ -24,15 +41,15 @@ btnNewBook.addEventListener('click', () => {
     txtISBN.focus();
 });
 
-function checkValidityOfISBN(){
+function checkValidityOfISBN() {
     return /^\d+$/.test(txtISBN.value);
 }
 
-function checkValidityOfName(){
+function checkValidityOfName() {
     return /^.+$/.test(txtName.value);
 }
 
-function checkValidityOfAuthor(){
+function checkValidityOfAuthor() {
     return /^[A-Za-z ]+$/.test(txtAuthor.value);
 }
 
@@ -40,14 +57,14 @@ txtISBN.addEventListener('input', checkValidity);
 txtName.addEventListener('input', checkValidity);
 txtAuthor.addEventListener('input', checkValidity);
 
-function checkValidity(e: Event){
+function checkValidity(e: Event) {
     (e.target as HTMLInputElement).classList.remove('is-valid', 'is-invalid');
-    if(e.target === txtISBN){
-        checkValidityOfISBN()? txtISBN.classList.add('is-valid'): txtISBN.classList.add('is-invalid');
-    }else if (e.target === txtName){
-        checkValidityOfName()? txtName.classList.add('is-valid'): txtName.classList.add('is-invalid');
-    }else{
-        checkValidityOfAuthor()? txtAuthor.classList.add('is-valid'): txtAuthor.classList.add('is-invalid');
+    if (e.target === txtISBN) {
+        checkValidityOfISBN() ? txtISBN.classList.add('is-valid') : txtISBN.classList.add('is-invalid');
+    } else if (e.target === txtName) {
+        checkValidityOfName() ? txtName.classList.add('is-valid') : txtName.classList.add('is-invalid');
+    } else {
+        checkValidityOfAuthor() ? txtAuthor.classList.add('is-valid') : txtAuthor.classList.add('is-invalid');
     }
 }
 
